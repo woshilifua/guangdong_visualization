82215c291150f60b02f632c03641986f986a6285
lifu <xiaofuai@gmail.com>
2020-02-29 01:04:26 +0800

    添加营销拓展样式

---
+++ src/views/marketing/components/RegionSelector.vue
@@ -0,0 +1,140 @@
+<template>
+  <el-row :gutter="10" type="flex" justify="start" align="middle">
+    <el-col :span="6">
+      <el-tag style="width: 100%;">广东省</el-tag>
+    </el-col>
+    <el-col :span="6">
+      <el-select
+        v-model="city"
+        value-key="adcode"
+        placeholder="市"
+        style="width: 100%;"
+        size="small"
+        @change="handleRegionSelectorChange(city)"
+      >
+        <el-option
+          v-for="item in cityList"
+          :key="item.adcode"
+          :label="item.name"
+          :value="item"
+        >
+        </el-option>
+      </el-select>
+    </el-col>
+    <el-col :span="6">
+      <el-select
+        v-model="district"
+        value-key="adcode"
+        placeholder="区"
+        style="width: 100%;"
+        size="small"
+        @change="handleRegionSelectorChange(district)"
+      >
+        <el-option
+          v-for="item in districtList"
+          :key="item.adcode"
+          :label="item.name"
+          :value="item"
+        >
+        </el-option>
+      </el-select>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import { getDistrict } from '@/api/map'
+
+export default {
+  props: {
+    region: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      province: '广东省',
+      cityList: [],
+      city: null,
+      districtList: [],
+      district: null
+    }
+  },
+
+  mounted() {
+    this.getDistrict()
+  },
+
+  computed: {
+    adCode() {
+      return this.region.adcode
+    }
+  },
+
+  watch: {
+    /*
+     * 监听 region 的 adcode 变化
+     */
+    adCode: {
+      handler() {
+        this.handleRegionChange(this.region)
+      },
+      immediate: false
+    }
+  },
+
+  methods: {
+    getDistrict() {
+      let params = {
+        key: process.env.VUE_APP_MAP_API_KEY,
+        keywords: '广东',
+        subdistrict: 3,
+        extensions: 'base'
+      }
+      getDistrict(params).then(rep => {
+        this.cityList = rep.districts[0].districts
+      })
+    },
+
+    handleRegionSelectorChange(region) {
+      this.$eventBus.$emit('change-region', {
+        adcode: Number(region.adcode),
+        level: region.level
+      })
+    },
+
+    // TODO bug 区域和地图没有对应上
+    handleRegionChange(region) {
+      if (region.level === 'province') {
+        // 到省份级别的时候，将市和区都清空
+        this.city = null
+        this.district = null
+        this.districtList = []
+      } else if (region.level === 'city') {
+        // 到市级别的时候将，将区清空，并找到对应的市
+        this.district = null
+        this.cityList.forEach(item => {
+          if (item.adcode === String(region.adcode)) {
+            this.districtList = item.districts
+            this.city = item
+          }
+        })
+      } else if (region.level === 'district') {
+        // 到区级别的时候，找到对应的区
+        this.districtList.forEach(item => {
+          if (item.adcode === String(region.adcode)) {
+            this.district = item
+          }
+        })
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.map-container {
+  margin-top: 20px;
+}
+</style>
--- src/views/marketing/expand.vue
+++ src/views/marketing/expand.vue
@@ -1,5 +1,68 @@
 <template>
   <div class="app-container">
-    营销拓展
+    <el-card shadow="never">
+      <el-row type="flex" align="middel" class="header">
+        <el-col :span="8">
+          <RegionSelector :region="region" style="height: 100%" />
+        </el-col>
+        <el-col :span="4">
+          <span>32002 家</span>
+          <span>客户总数</span>
+        </el-col>
+        <el-col :span="4">
+          <span>300,000 元</span>
+          <span>收入总数</span>
+        </el-col>
+        <el-col :span="4">
+          <span>12 元</span>
+          <span>平均客户收入</span>
+        </el-col>
+        <el-col :span="4">
+          <span>第二</span>
+          <span>平均客户收入排名</span>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
+<script>
+import RegionSelector from './components/RegionSelector'
+
+export default {
+  components: {
+    RegionSelector
+  },
+
+  data() {
+    return {
+      region: {
+        level: 'province',
+        adcode: 440000,
+        center: [113.280637, 23.125178]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-container {
+  min-height: 100%;
+  background-color: #f2f3f8;
+}
+.header span {
+  text-align: left;
+  display: block;
+  line-height: 24px;
+  font-size: 14px;
+}
+.header span:first-of-type {
+  color: #022c54;
+  font-size: 20px;
+  font-weight: 500;
+  margin-bottom: 6px;
+}
+.bl {
+  border-left: 1px solid #022c54;
+}
+</style>
